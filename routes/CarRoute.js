7a54b892fea983d79253b3a1c52e7d39e63d4e38
William <williambecker321@gmail.com>
2021-10-07 10:22:28 +0200

    Har försökt skicka data  för bara en bil.

--- routes/CarRoute.js
+++ routes/CarRoute.js
@@ -7,5 +7,6 @@ const router = express.Router();
 const Cars_controler = require('../controler/CarsControler');
 
 router.get('/data', Cars_controler.carData);
+router.get('/:Id', Cars_controler.carId);
 
 module.exports = router;
